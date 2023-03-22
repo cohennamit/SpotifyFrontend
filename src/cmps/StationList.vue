3a8bd8b5a6c1d9fb6475c13f7b00f1d103d3142e
G1320 <darnellgreen211@gmail.com>
2023-03-22 10:14:06 +0200

    Started adding components, added imgs to station data

---
+++ src/cmps/StationList.vue
@@ -0,0 +1,19 @@
+<template></template>
+<StationList v-if="stations" :stations="stations" @removed="removeStation" :class="'station-list'" />
+
+<script>
+export default {
+  name: '',
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  created() {},
+  components: {
+    ToyList,
+  },
+};
+</script>
+
+<style></style>
--- src/services/station.service.local.js
+++ src/services/station.service.local.js
@@ -1,79 +1,99 @@
+import { storageService } from './async-storage.service.js';
+import { utilService } from './util.service.js';
+import { userService } from './user.service.js';
 
-import { storageService } from './async-storage.service.js'
-import { utilService } from './util.service.js'
-import { userService } from './user.service.js'
-
-const STORAGE_KEY = 'station'
+const STORAGE_KEY = 'station';
 
 export const stationService = {
-    query,
-    getById,
-    save,
-    remove,
-    getEmptyStation,
-    addStationMsg
-}
-window.cs = stationService
-
+  query,
+  getById,
+  save,
+  remove,
+  getEmptyStation,
+  addStationMsg,
+};
+window.cs = stationService;
 
 async function query(filterBy = { txt: '', listencount: 0 }) {
-    var stations = await storageService.query(STORAGE_KEY)
-    if (filterBy.txt) {
-        const regex = new RegExp(filterBy.txt, 'i')
-        stations = stations.filter(station => regex.test(station.title) || regex.test(station.description))
-    }
-    if (filterBy.listencount) {
-        stations = stations.filter(station => station.listencount <= filterBy.listencount)
-    }
-    return stations
+  var stations = await storageService.query(STORAGE_KEY);
+  if (filterBy.txt) {
+    const regex = new RegExp(filterBy.txt, 'i');
+    stations = stations.filter(
+      (station) => regex.test(station.title) || regex.test(station.description)
+    );
+  }
+  if (filterBy.listencount) {
+    stations = stations.filter((station) => station.listencount <= filterBy.listencount);
+  }
+  return stations;
 }
 
 function getById(stationId) {
-    return storageService.get(STORAGE_KEY, stationId)
+  return storageService.get(STORAGE_KEY, stationId);
 }
 
 async function remove(stationId) {
-    await storageService.remove(STORAGE_KEY, stationId)
+  await storageService.remove(STORAGE_KEY, stationId);
 }
 
 async function save(station) {
-    var savedStation
-    if (station._id) {
-        savedStation = await storageService.put(STORAGE_KEY, station)
-    } else {
-        // Later, owner is set by the backend
-        station.owner = userService.getLoggedinUser()
-        savedStation = await storageService.post(STORAGE_KEY, station)
-    }
-    return savedStation
+  var savedStation;
+  if (station._id) {
+    savedStation = await storageService.put(STORAGE_KEY, station);
+  } else {
+    // Later, owner is set by the backend
+    station.owner = userService.getLoggedinUser();
+    savedStation = await storageService.post(STORAGE_KEY, station);
+  }
+  return savedStation;
 }
 
 async function addStationMsg(stationId, txt) {
-    // Later, this is all done by the backend
-    const station = await getById(stationId)
-    if (!station.msgs) station.msgs = []
+  // Later, this is all done by the backend
+  const station = await getById(stationId);
+  if (!station.msgs) station.msgs = [];
 
-    const msg = {
-        id: utilService.makeId(),
-        by: userService.getLoggedinUser(),
-        txt
-    }
-    station.msgs.push(msg)
-    await storageService.put(STORAGE_KEY, station)
+  const msg = {
+    id: utilService.makeId(),
+    by: userService.getLoggedinUser(),
+    txt,
+  };
+  station.msgs.push(msg);
+  await storageService.put(STORAGE_KEY, station);
 
-    return msg
+  return msg;
 }
 
 function getEmptyStation() {
-    return {
-        title: 'Susita-' + (Date.now() % 1000),
-        listencount: utilService.getRandomIntInclusive(1000, 9000),
-    }
+  return {
+    title: 'Susita-' + (Date.now() % 1000),
+    listencount: utilService.getRandomIntInclusive(1000, 9000),
+  };
 }
 
-
-// TEST DATA
-// ; (async () => {
-//     await storageService.post(STORAGE_KEY, { title: 'Michael Jackson', listencount: 420 })
-//     await storageService.post(STORAGE_KEY, { title: 'Shakira', listencount: 21 })
-// })()
+// (async () => {
+//   await storageService.post(STORAGE_KEY, {
+//     title: 'Michael Jackson',
+//     listencount: 420,
+//     imgUrl: 'https://source.unsplash.com/random/?cats&4',
+//     createdAt: Date.now(),
+//   });
+//   await storageService.post(STORAGE_KEY, {
+//     title: 'Shakira',
+//     listencount: 21,
+//     imgUrl: 'https://source.unsplash.com/random/?cats&4',
+//     createdAt: Date.now(),
+//   });
+//   await storageService.post(STORAGE_KEY, {
+//     title: 'Biggy',
+//     listencount: 221,
+//     imgUrl: 'https://source.unsplash.com/random/?cats&4',
+//     createdAt: Date.now(),
+//   });
+//   await storageService.post(STORAGE_KEY, {
+//     title: 'Flock of seagulls',
+//     listencount: 2221,
+//     createdAt: Date.now(),
+//     imgUrl: 'https://source.unsplash.com/random/?cats&4',
+//   });
+// })();
