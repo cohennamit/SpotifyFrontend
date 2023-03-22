7df8aa2d11676b6865b76036fbec9fa691a5331d
G1320 <darnellgreen211@gmail.com>
2023-03-22 14:10:20 +0200

    Designed main layout

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,38 @@
 <template>
   <section>
-    <AppHeader />
-    <AppHeader2 />
-    <MainNav />
-    <RouterView />
-    <AppFooter />
-    <UserMsg />
+    <main class="app-main">
+      <!-- <AppHeader /> -->
+      <MainNav />
+      <AppHeader2 />
+      <RouterView />
+      <AppFooter />
+      <UserMsg />
+    </main>
   </section>
 </template>
 
 <script>
+import { userService } from './services/user.service';
+import { store } from './store/store';
 
-
-import { userService } from './services/user.service'
-import { store } from './store/store'
-
-import AppHeader from './cmps/AppHeader.vue'
-import AppHeader2 from './cmps/AppHeader2.vue'
-import MainNav from './cmps/MainNav.vue'
-import AppFooter from './cmps/AppFooter.vue'
-import UserMsg from './cmps/UserMsg.vue'
-
+import AppHeader from './cmps/AppHeader.vue';
+import AppHeader2 from './cmps/AppHeader2.vue';
+import MainNav from './cmps/MainNav.vue';
+import AppFooter from './cmps/AppFooter.vue';
+import UserMsg from './cmps/UserMsg.vue';
 
 export default {
-
   created() {
-    console.log('Vue App created')
-    const user = userService.getLoggedinUser()
-    if (user) store.commit({ type: 'setLoggedinUser', user })
+    console.log('Vue App created');
+    const user = userService.getLoggedinUser();
+    if (user) store.commit({ type: 'setLoggedinUser', user });
   },
   components: {
     AppHeader,
     AppHeader2,
     MainNav,
     AppFooter,
-    UserMsg
+    UserMsg,
   },
-}
+};
 </script>
