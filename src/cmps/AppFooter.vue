81bbafe9e383276f57ea15e37cb8dbfa13215877
Amit Cohen <cohennamitt@gmail.com>
2023-03-22 09:46:26 +0200

    improved footer

--- src/cmps/AppFooter.vue
+++ src/cmps/AppFooter.vue
@@ -1,11 +1,11 @@
 <template>
     <section class="footer">
         <h1>footer</h1>
-        <button class="footer-btns">Shuffle</button>
-        <button class="footer-btns">Prev</button>
-        <button class="footer-btns">Pause</button>
-        <button class="footer-btns">Next</button>
-        <button class="footer-btns">Repeat</button>
+        <button class="footer-btn">Shuffle</button>
+        <button class="footer-btn">Prev</button>
+        <button class="footer-btn">Pause</button>
+        <button class="footer-btn">Next</button>
+        <button class="footer-btn">Repeat</button>
         <input class="footer-volume" type="range" min="0" max="1" step="0.1" value="1" id="volume-slider">
     </section>
 </template>
