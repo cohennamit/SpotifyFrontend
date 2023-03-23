3c27151af0555b945df19fb1b0637fcc4f11e721
G1320 <darnellgreen211@gmail.com>
2023-03-23 20:08:34 +0200

    Added Song fiter comp

--- src/cmps/SongFilter.vue
+++ src/cmps/SongFilter.vue
@@ -1,18 +1,69 @@
 <template>
-  <h1>Test</h1>
+  <section class="song-filter">
+    <form @submit.prevent="search">
+      <input type="search" v-model="query" placeholder="Enter search term" />
+      <button type="submit">Search</button>
+    </form>
+    <div v-if="loading">Loading...</div>
+    <div v-else>
+      <ul>
+        <li v-for="(video, index) in videos" :key="index">
+          <h4>{{ index + 1 }}</h4>
+          <img :src="video.snippet.thumbnails.default.url" alt="" />
+          <div>
+            <h3>{{ shortenedTitle(video.snippet.title) }}</h3>
+
+            <h6>Artist</h6>
+          </div>
+          <iframe
+            hidden
+            width="60"
+            height="15"
+            :src="'https://www.youtube.com/embed/' + video.id.videoId"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        </li>
+      </ul>
+      <div v-if="!videos.length && !loading">No Songs</div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-  name: '',
   data() {
-    return {};
+    return {
+      query: '',
+      videos: [],
+      loading: false,
+    };
   },
-  methods: {},
-  computed: {},
-  created() {},
-  components: {},
+  computed: {
+    shortenedTitle() {
+      const maxLength = 15;
+      return function (title) {
+        return title.length > maxLength ? title.slice(0, maxLength) + '...' : title;
+      };
+    },
+  },
+  methods: {
+    async search() {
+      const API_KEY = 'AIzaSyB_6u19ZnSR_5zv7HYgTJKw6qkPpnsREcg';
+      this.videos = [];
+
+      try {
+        const response = await fetch(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${this.query}&type=video&maxResults=10&key=${API_KEY}`
+        );
+        const { items } = await response.json();
+        console.log('items: ', items);
+        console.log();
+        this.videos = items;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
 };
 </script>
-
-<style></style>
--- src/views/StationDetails.vue
+++ src/views/StationDetails.vue
@@ -1,59 +1,58 @@
 <template>
-    <section class="station-details">
-        <StationHeader :station="station" />
-        <!-- <SongList> </SongList> -->
-    </section>
+  <section class="station-details">
+    <StationHeader :station="station" />
+    <!-- <SongList> </SongList> -->
     <article class="station-details-body">
-        <div @click="toggleStationOptions" class="icon" v-html="getSvg('playlistOptions')"></div>
-        <ul v-if="isOptionsShown">
-            <li v-if="station.isAddedByUser" @click="onEditStation">Edit</li>
-            <li v-if="station.isAddedByUser" @click="onRemoveStation">Delete</li>
-        </ul>
+      <div @click="toggleStationOptions" class="icon" v-html="getSvg('playlistOptions')"></div>
+      <ul v-if="isOptionsShown">
+        <li v-if="station.isAddedByUser" @click="onEditStation">Edit</li>
+        <li v-if="station.isAddedByUser" @click="onRemoveStation">Delete</li>
+      </ul>
     </article>
+    <SongFilter />
+  </section>
 </template>
 
 <script>
 //TODO: LINE 2-4 CHANGE IMG TO BE CHOSEN BY USER & CONNECT USER LINE 9
-import { stationService } from '../services/station.service.local.js'
-import { svgService } from '../services/svg.service.js'
-import  StationHeader  from '../cmps/StationHeader.vue'
+import { stationService } from '../services/station.service.local.js';
+import { svgService } from '../services/svg.service.js';
+import StationHeader from '../cmps/StationHeader.vue';
+import SongFilter from '../cmps/SongFilter.vue';
 export default {
-    name: '',
-    data() {
-        return {
-            station: {},
-            isOptionsShown: false
-        }
+  name: '',
+  data() {
+    return {
+      station: {},
+      isOptionsShown: false,
+    };
+  },
+  methods: {
+    getSvg(iconName) {
+      return svgService.getSvg(iconName);
     },
-    methods: {
-        getSvg(iconName) {
-            return svgService.getSvg(iconName)
-        },
-        toggleStationOptions() {
-            this.isOptionsShown = !this.isOptionsShown
-        },
-        onRemoveStation(){
-            this.$store.dispatch({type:'removeStation', stationId: this.station._id})
-            this.$router.push('/station')
-        }
+    toggleStationOptions() {
+      this.isOptionsShown = !this.isOptionsShown;
     },
-    computed: {
-
+    onRemoveStation() {
+      this.$store.dispatch({ type: 'removeStation', stationId: this.station._id });
+      this.$router.push('/station');
     },
-    async created() {
-        const { stationId } = this.$route.params;
-        // console.log('create',stationId)
-        try {
-            const station = await stationService.getById(stationId);
-            this.station = station;
-        } catch (error) {
-            console.log('Error fetching station: ', error);
-        }
-    },
-    components: {
-        StationHeader,
-    },
-
-}
+  },
+  computed: {},
+  async created() {
+    const { stationId } = this.$route.params;
+    // console.log('create',stationId)
+    try {
+      const station = await stationService.getById(stationId);
+      this.station = station;
+    } catch (error) {
+      console.log('Error fetching station: ', error);
+    }
+  },
+  components: {
+    StationHeader,
+    SongFilter,
+  },
+};
 </script>
-
