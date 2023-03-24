03727863a3f6135d3b64f63e33a21b6002af4f03
G1320 <darnellgreen211@gmail.com>
2023-03-24 12:39:51 +0300

    Working on Song filter comp

--- src/cmps/StationPreview.vue
+++ src/cmps/StationPreview.vue
@@ -2,22 +2,10 @@
   <section @click="goToDetails" class="station-preview">
     <img v-if="station.imgUrl" :src="station.imgUrl" />
     <p>{{ station.title }}</p>
-
-    <!-- <p>Listen Count : {{ station.listencount?.toLocaleString() }}</p> -->
-    <!-- <button @click="removeStation(station._id)">x</button>
-    <button @click="updateStation(station)">Update</button>
-    <hr />
-    <button @click="addStationMsg(station._id)">Add station msg</button>
-    <button @click="printStationToConsole(station)">Print msgs to console</button> -->
   </section>
 </template>
 
 <script>
-import {
-  getActionRemoveStation,
-  getActionUpdateStation,
-  getActionAddStationMsg,
-} from '../store/station.store';
 export default {
   name: '',
   props: {
@@ -27,42 +15,11 @@ export default {
     return {};
   },
   methods: {
-    goToDetails(){
-      this.$router.push(`/station/${this.station._id}`)
-    },
-    async removeStation(stationId) {
-      try {
-        await this.$store.dispatch(getActionRemoveStation(stationId));
-        showSuccessMsg('Station removed');
-      } catch (err) {
-        console.log(err);
-        showErrorMsg('Cannot remove station');
-      }
-    },
-    async updateStation(station) {
-      try {
-        station = { ...station };
-        station.listencount = +prompt('New listencount?', station.listencount);
-        await this.$store.dispatch(getActionUpdateStation(station));
-        showSuccessMsg('Station updated');
-      } catch (err) {
-        console.log(err);
-        showErrorMsg('Cannot update station');
-      }
-    },
-    async addStationMsg(stationId) {
-      try {
-        await this.$store.dispatch(getActionAddStationMsg(stationId));
-        showSuccessMsg('Station msg added');
-      } catch (err) {
-        console.log(err);
-        showErrorMsg('Cannot add station msg');
-      }
-    },
-    printStationToConsole(station) {
-      console.log('Station msgs:', station.msgs);
+    goToDetails() {
+      this.$router.push(`/station/${this.station._id}`);
     },
   },
+
   computed: {},
   created() {},
   components: {},
