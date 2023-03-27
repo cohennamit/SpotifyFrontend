79eb7f5704ae670929df048d29aa2d798915936c
ilanSuli <ilansuli2147@gmail.com>
2023-03-27 10:17:35 +0300

    improved playlist details page styling

--- src/cmps/PlayBtn.vue
+++ src/cmps/PlayBtn.vue
@@ -1,6 +1,6 @@
 <template>
 <button class="play-btn">
-    <div  class="play-btn-icon" v-html="getSvg('playSong')"></div>
+    <div  class="play-btn-icon" v-html="getSvg('playSongLarge')"></div>
 </button>
 </template>
 
