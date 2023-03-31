0c0899068f89e87d1a8284eaf8f29180624ccc46
Amit Cohen <cohennamitt@gmail.com>
2023-03-31 22:17:04 +0300

    media querys and ui fixes

--- src/services/songService.js
+++ src/services/songService.js
@@ -1,8 +1,8 @@
 export async function getSongs(query) {
-    const API_KEY = 'AIzaSyDwuqg_6XzAtN5YBW02uFmzbFyj1pflXcY';
+    const API_KEY = 'AIzaSyB0I22L8tDCu7ElQf0C0vaGQz5mV5P6K9s';
     try {
         const response = await fetch(
-            `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=1&key=${API_KEY}`
+            `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=10&key=${API_KEY}`
             // `https://www.googleapis.com/youtube/v3/search?part=snippet,contentDetails&q=${this.query}&type=video&maxResults=10&key=${API_KEY}`
         );
         const { items } = await response.json();
